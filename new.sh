49d4f261bf3333fae72709687e65addaf4bc05c3
isaacs <i@foohack.com>
2009-03-23 16:48:13 -0700

    Automatically add the title, create the nice filename.

--- new.sh
+++ new.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ! [ -d `date '+%Y-%m-%d'` ] && mkdir `date '+%Y-%m-%d'`
 read -p "Title? " t
-$EDITOR "`date '+%Y-%m-%d'`/$t.markdown"
+echo $t | tr '[:upper:]' '[:lower:]' | perl -pi -e 's/[^a-z0-9-]+/-/g;s/-+/-/g;s/^-|-$//g' | read f
+! [ -f "`date '+%Y-%m-%d'`/$f.markdown" ] && echo "# $t" > "`date '+%Y-%m-%d'`/$f.markdown"
+$EDITOR "`date '+%Y-%m-%d'`/$f.markdown"
