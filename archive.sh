16c1327bd903aec88d13e8dd4e81cda6d426cb5c
isaacs <i@foohack.com>
2009-03-29 03:02:07 -0700

    Be more clever.

--- archive.sh
+++ archive.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-git stash
+didstash=0
+git stash && didstash=1
 (
-cat header.markdown 2>/dev/null
-ls -F \
-|egrep '[0-9]{4}(-[0-9]{2}){2}/$'\
-|sort -r\
-|sed 's|/$||'\
-|while read DATE; do
-	cd $DATE &>/dev/null
-	diddate=0
-	ls -trF | egrep /$ | while read POST; do
-		[ $diddate == 0 ] && echo "" && echo "## $DATE" && let 'diddate ++'
-		cd $POST &>/dev/null
-		pl=$DATE/$POST
-		e=${pl//\//\\/}
-		e=${e//\$/\\\$}
-		echo ""
-		echo [$pl]: $pl
-		cat README.markdown\
-		| perl -pi -e 's/^#([^#].*)$/#$1 [∞]['$e']/'\
-		| sed 's|^#|###|g'
+	cat header.markdown 2>/dev/null
+	ls -F \
+	|egrep '[0-9]{4}(-[0-9]{2}){2}/$'\
+	|sort -r\
+	|sed 's|/$||'\
+	|while read DATE; do
+		cd $DATE &>/dev/null
+		diddate=0
+		ls -trF | egrep /$ | while read POST; do
+			[ $diddate == 0 ] && echo "" && echo "## $DATE" && let 'diddate ++'
+			cd $POST &>/dev/null
+			pl=$DATE/$POST
+			e=${pl//\//\\/}
+			e=${e//\$/\\\$}
+			echo ""
+			echo [$pl]: $pl
+			cat README.markdown\
+			| perl -pi -e 's/^#([^#].*)$/#$1 [∞]['$e']/'\
+			| sed 's|^#|###|g'
+		done
 	done
-done
 ) > README.markdown \
-&& git add README.markdown &&\
-git commit -m "Generate archive"
+&& git add README.markdown\
+&& git commit -m "Generate archive"
+[ $didstash -eq 1 ] && git stash pop
