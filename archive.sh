12dfa806b282bc796c5969a4facf7f9980d551c0
isaacs <i@foohack.com>
2009-03-29 02:44:20 -0700

    A better archive logic.

--- archive.sh
+++ archive.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 git stash
 (
-cat README.markdown
+cat header.markdown 2>/dev/null
 ls -F \
 |egrep '[0-9]{4}(-[0-9]{2}){2}/$'\
 |sort -r\
 |sed 's|/$||'\
 |while read DATE; do
 	cd $DATE &>/dev/null
-	echo ""
-	echo "## $DATE"
+	diddate=0
 	ls -trF | egrep /$ | while read POST; do
+		[ $diddate == 0 ] && echo "" && echo "## $DATE" && let 'diddate ++'
 		cd $POST &>/dev/null
-		pl=../$DATE/$POST
+		pl=$DATE/$POST
 		e=${pl//\//\\/}
 		e=${e//\$/\\\$}
 		echo ""
@@ -22,6 +22,6 @@ ls -F \
 		| sed 's|^#|###|g'
 	done
 done
-) > archive/README.markdown \
-&& git add archive/README.markdown &&\
+) > README.markdown \
+&& git add README.markdown &&\
 git commit -m "Generate archive"
