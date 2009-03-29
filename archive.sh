79677f93655fbe7f765a4a2debc04e9970ac87aa
isaacs <i@foohack.com>
2009-03-28 22:24:30 -0700

    The archiver (this time, don't rebase it away!)

---
+++ archive.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+git stash
+(
+cat README.markdown
+ls -F \
+|egrep '[0-9]{4}(-[0-9]{2}){2}/$'\
+|sort -r\
+|sed 's|/$||'\
+|while read DATE; do
+	cd $DATE &>/dev/null
+	echo ""
+	echo "## $DATE"
+	ls -trF | egrep /$ | while read POST; do
+		cd $POST &>/dev/null
+		pl=../$DATE/$POST
+		e=${pl//\//\\/}
+		e=${e//\$/\\\$}
+		echo ""
+		echo [$pl]: $pl
+		cat README.markdown\
+		| perl -pi -e 's/^#([^#].*)$/#$1 [∞]['$e']/'\
+		| sed 's|^#|###|g'
+	done
+done
+) > archive/README.markdown
+&& git add archive/README.markdown &&\
+git commit -m "Generate archive"
