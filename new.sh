bb258895ca57b58f6a5f229efef1c2b3ea6d0820
isaacs <i@foohack.com>
2009-03-28 19:18:41 -0700

    Change to a new structure to take advantage of github's automagic reading of README.markdown files. Also, a snip commenting on the change, and on this commit message which is commenting on it and the change.

--- new.sh
+++ new.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ! [ -d `date '+%Y-%m-%d'` ] && mkdir `date '+%Y-%m-%d'`
 read -p "Title? " t
-echo $t | tr '[:upper:]' '[:lower:]' | perl -pi -e 's/[^a-z0-9-]+/-/g;s/-+/-/g;s/^-|-$//g' | read f
-! [ -f "`date '+%Y-%m-%d'`/$f.markdown" ] && echo "# $t" > "`date '+%Y-%m-%d'`/$f.markdown"
-$EDITOR "`date '+%Y-%m-%d'`/$f.markdown"
+<<< $t tr '[:upper:]' '[:lower:]' | perl -pi -e 's/[^a-z0-9-]+/-/g;s/-+/-/g;s/^-|-$//g' | read f
+! [ -d "`date '+%Y-%m-%d'`/$f" ] && mkdir -p "`date '+%Y-%m-%d'`/$f"
+! [ -f "`date '+%Y-%m-%d'`/$f/README.markdown" ] && echo "# $t" > "`date '+%Y-%m-%d'`/$f/README.markdown"
+$EDITOR "`date '+%Y-%m-%d'`/$f/README.markdown"
