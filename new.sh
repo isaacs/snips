5300536d76abf6ea3cb791320c51134af11de80e
isaacs <i@foohack.com>
2009-03-28 19:42:16 -0700

    Move new structure to its proper place.  Fix new.sh so that it doesn't keep breaking.

--- new.sh
+++ new.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 ! [ -d `date '+%Y-%m-%d'` ] && mkdir `date '+%Y-%m-%d'`
 read -p "Title? " t
-<<< $t tr '[:upper:]' '[:lower:]' | perl -pi -e 's/[^a-z0-9-]+/-/g;s/-+/-/g;s/^-|-$//g' | read f
-! [ -d "`date '+%Y-%m-%d'`/$f" ] && mkdir -p "`date '+%Y-%m-%d'`/$f"
-! [ -f "`date '+%Y-%m-%d'`/$f/README.markdown" ] && echo "# $t" > "`date '+%Y-%m-%d'`/$f/README.markdown"
-$EDITOR "`date '+%Y-%m-%d'`/$f/README.markdown"
+<<< $t tr '[:upper:]' '[:lower:]'\
+|perl -pi -e 's/[^a-z0-9-]+/-/g;s/-+/-/g;s/^-|-$//g'\
+|(cat; echo "")\
+|while read f; do
+	! [ -d "`date '+%Y-%m-%d'`/$f" ] && mkdir -p "`date '+%Y-%m-%d'`/$f"
+	! [ -f "`date '+%Y-%m-%d'`/$f/README.markdown" ] && echo "# $t" > "`date '+%Y-%m-%d'`/$f/README.markdown"
+	$EDITOR "`date '+%Y-%m-%d'`/$f/README.markdown"
+done
+
+
